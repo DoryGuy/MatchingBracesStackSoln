58c1933fba459a35592ec278795525a104016fbc
DoryGuy <gwpowell@hotmail.com>
2025-02-02 17:46:41 -0800

    fix enum to python standards.

--- main.py
+++ main.py
@@ -6,106 +6,118 @@
 #  Copyright © 2025 Guest User. All rights reserved.
 #
 
+# pylint: disable=line-too-long,missing-module-docstring
+
 from enum import Enum
 
-PASS = 1
-FAIL = 0
-enumText = ('FAIL', 'PASS')
-
-Result_t = Enum ('Result',  [(enumText[FAIL], FAIL), (enumText[PASS], PASS) ])
+class ResultType(Enum):
+    '''
+    Class to have a type for results which is type safe and
+    easy to print. ie builtin 'name'
+    '''
+    PASS = 1
+    FAIL = 0
 
 # character set which starts the grouping
 opening = { '(', '{', '[', '<' }
 # hash map which maps opening character to the closing character.
-match_brace = { ')': '(', 
+match_brace = { ')': '(',
                 '}': '{',
                 ']': '[', 
                 '>': '<' }
 
-def parse(input):
+def parse(string: str) -> ResultType:
+    '''
+    check the input for correctly matching braces
+    '''
     braces = []
-    
-    for c in input:
+
+    for c in string:
         if c in opening:
-            braces.append(c);
+            braces.append(c)
         else:
             if match_brace.get(c) is not None:
                 found_closing = match_brace.get(c)
                 if len(braces) == 0:
-                    return FAIL;
-                p = braces[-1];
-                braces.pop();
-                if (p != found_closing):
-                    return FAIL;
+                    return ResultType.FAIL
+                p: str = braces[-1]
+                braces.pop()
+                if p != found_closing:
+                    return ResultType.FAIL
 
     if len(braces) != 0:
-        return FAIL;
-    
-    return PASS;
+        return ResultType.FAIL
 
-def main():
-    input = (
-        ("([])", PASS),
-        ("(abc)", PASS),
-        ("((", FAIL),
-        (")(", FAIL),
-        ("({[]})", PASS),
-        ("", PASS),
-        (")", FAIL),
-        ("]", FAIL),
-        ("}", FAIL),
-        ("(", FAIL),
-        ("[", FAIL),
-        ("}", FAIL),
-        ("<", FAIL),
-        ("({)", FAIL),
-        ("(})", FAIL),
-        ("([)", FAIL),
-        ("(])", FAIL),
-        ("({)", FAIL),
-        ("[(]", FAIL),
-        ("[)]", FAIL),
-        ("[{]", FAIL),
-        ("[{]}", FAIL),
-        ("({])", FAIL),
-        ("([})", FAIL),
-        ("((]}", FAIL),
-        ("([)]", FAIL),
-        ("({)}", FAIL),
-        ("[{]}", FAIL),
-        ("[(])", FAIL),
-        ("{(})", FAIL),
-        ("{[}]", FAIL),
-        ("<(>)", FAIL),
-        ("<[>]", FAIL),
-        ("<{>}", FAIL),
-        ("(([))]", FAIL),
-        ("{(}", FAIL),
-        ("{[}", FAIL),
-        ("()(", FAIL),
-        ("][", FAIL),
-        ("}{", FAIL),
-        ("()", PASS),
-        ("[]", PASS),
-        ("{}", PASS),
-        ("<>", PASS),
-        ("<()>", PASS),
-        ("><", FAIL),
-        ("()[]", PASS),
-        ("(){}", PASS),
-        ("[]{}", PASS),
-        ("()[]{}", PASS),
-        ("()[]{}<>", PASS)
+    return ResultType.PASS
+
+def main() -> int:
+    '''
+    controlling fn.
+    '''
+    test_strings = (
+        ("([])", ResultType.PASS),
+        ("(abc)", ResultType.PASS),
+        ("((", ResultType.FAIL),
+        (")(", ResultType.FAIL),
+        ("({[]})", ResultType.PASS),
+        ("", ResultType.PASS),
+        (")", ResultType.FAIL),
+        ("]", ResultType.FAIL),
+        ("}", ResultType.FAIL),
+        ("(", ResultType.FAIL),
+        ("[", ResultType.FAIL),
+        ("}", ResultType.FAIL),
+        ("<", ResultType.FAIL),
+        ("({)", ResultType.FAIL),
+        ("(})", ResultType.FAIL),
+        ("([)", ResultType.FAIL),
+        ("(])", ResultType.FAIL),
+        ("({)", ResultType.FAIL),
+        ("[(]", ResultType.FAIL),
+        ("[)]", ResultType.FAIL),
+        ("[{]", ResultType.FAIL),
+        ("[{]}", ResultType.FAIL),
+        ("({])", ResultType.FAIL),
+        ("([})", ResultType.FAIL),
+        ("((]}", ResultType.FAIL),
+        ("([)]", ResultType.FAIL),
+        ("({)}", ResultType.FAIL),
+        ("[{]}", ResultType.FAIL),
+        ("[(])", ResultType.FAIL),
+        ("{(})", ResultType.FAIL),
+        ("{[}]", ResultType.FAIL),
+        ("<(>)", ResultType.FAIL),
+        ("<[>]", ResultType.FAIL),
+        ("<{>}", ResultType.FAIL),
+        ("(([))]", ResultType.FAIL),
+        ("{(}", ResultType.FAIL),
+        ("{[}", ResultType.FAIL),
+        ("()(", ResultType.FAIL),
+        ("][", ResultType.FAIL),
+        ("}{", ResultType.FAIL),
+        ("()", ResultType.PASS),
+        ("[]", ResultType.PASS),
+        ("{}", ResultType.PASS),
+        ("<>", ResultType.PASS),
+        ("<()>", ResultType.PASS),
+        ("><", ResultType.FAIL),
+        ("()[]", ResultType.PASS),
+        ("(){}", ResultType.PASS),
+        ("[]{}", ResultType.PASS),
+        ("()[]{}", ResultType.PASS),
+        ("()[]{}<>", ResultType.PASS)
     )
 
-    for i in input:
+    for i in test_strings:
         result = parse(i[0])
-        if (result == i[1]):
-            print("input = \"" + i[0] + "\" correctly " + enumText[result] + ".")
+        if result == i[1]:
+            print("input = \"" + i[0] + "\" correctly " + result.name + ".")
         else:
-            print("input " + enumText[result] + " \"" + i[0] + "\" should should have " + enumText[i[1]] + ".")
+            print("input " + result.name + " \"" + i[0] + "\" should should have " + i[1].name + ".")
 
+    print("Done")
     return 0
 
-main()
-print("Done")
+# This code runs only when the module is executed directly
+if __name__ == '__main__':
+    main()
